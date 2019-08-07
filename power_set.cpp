a1f37aa225945b8c643f518bd3d67b123025baf4
Farhan Ansari <farhanahraf03@gmail.com>
2019-08-07 19:01:28 +0530

    Update power_set.cpp

--- power_set.cpp
+++ power_set.cpp
@@ -1,53 +1,71 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-void subarray(vector<int>, int , int);
+
+void powerSet(vector<int>, int);
+
+void disp(vector<int>);
 
 int main()
 {
-
+vector<int> v;
 int n;
-cout<<"Enter the size of the vector:";cin>>n;
+
+cout<<"\nEnter size of array: ";cin>>n;
+
 int temp;
-
-vector<int>v;
-
 for(int i=0; i<n; i++)
 {
 	cin>>temp;
 	v.push_back(temp);
 }
 
+powerSet(v,n);
 
-subarray(v,0,0);
 
 
 return 0;
 }
 
-void subarray(vector<int> v, int start, int end)
+
+void powerSet(vector<int> v, int n)
 {
-
-	if(end == v.size())return;
-	
-	if(start > end)
-	{
-		subarray(v,0,end+1);
-	}
-	
-	else
-	{
-		cout<<endl<<"[";
-		for(int i=start; i<end; i++)
-		{
-			cout<<v.at(i)<<",";
-		}
-		
-		cout<<v.at(end)<<"]"<<endl;
-		
-		subarray(v,start+1,end);
-	}
-
-
+ int total = pow(2,n);
+ 
+ vector<int> op;
+ 
+ for(int i=1; i<total; i++)
+ {
+ 	int j;
+ 	for(j=0; j<n; j++)
+ 	{
+ 		if(i & 1<<j)op.push_back(v.at(j));
+ 	}
+ 	
+ 	disp(op);
+ 	
+ 	op.clear();
+ 	
+ 	cout<<endl;
+ }
+ 
+ return;
 }
+
+
+
+void disp(vector<int> v)
+{
+	int i;
+	cout<<"[";
+	for( i=0; i<v.size()-1; i++)
+	{
+		cout<<v.at(i)<<",";
+	}
+	cout<<v.at(i)<<"]";
+}
+
